005d0a35b74d8d6bcb3df6d0b1a271c391c1d9ff
MsMapple <81583351+MsMapple@users.noreply.github.com>
2022-04-18 11:33:58 -0300

    floreria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquiposComponent } from './page/equipos/equipos.component';
+import { InicioComponent } from './page/inicio/inicio.component';
+import { ServiciosComponent } from './page/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component:InicioComponent},
+  {path:'servicio', component:ServiciosComponent},
+  {path:'equipo', component:EquiposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './page/inicio/inicio.component';
+import { EquipoComponent } from './page/equipo/equipo.component';
+import { ServiciosComponent } from './page/servicios/servicios.component';
+import { EquiposComponent } from './page/equipos/equipos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    EquipoComponent,
+    ServiciosComponent,
+    EquiposComponent
   ],
   imports: [
     BrowserModule,
